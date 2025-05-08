b6a9fb0b14c7c1fe6ae751138cf72e51c8382b24
masa-mocchi <158788104+masa-mocchi@users.noreply.github.com>
2025-05-08 18:38:05 +0900

    add: Modelを作成し、データベースのテーブルとマッピングしてレコードを全件取得できるようにコントローラーに記述

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers;
 
+// 追加
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        // dd('Hello World!');
-        return view('todo.index'); // 修正
+        // 追加
+        $todo = new Todo();
+        $todos = $todo->all();
+        // dd($todos);
+        // TodoModelを介して、todosテーブルのデータの全件取得を確認できたためddは削除
+
+        return view('todo.index');
     }
 }
+
